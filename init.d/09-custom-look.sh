55eded31e91476dc2ead17419dde9435543515e7
Vincent Fazio <vjf@users.noreply.github.com>
2023-06-30 09:19:58 +0000

    Added AAF and AuScope logo icon

--- init.d/09-custom-look.sh
+++ init.d/09-custom-look.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Disallow creating local accounts from the UI
 curl -X PUT -d 'false' http://localhost:8080/api/admin/settings/:AllowSignUp
-curl -X PUT -d google http://localhost:8080/api/admin/settings/:DefaultAuthProv
-ider
+
+# Set aaf to be the default authentication provider
+curl -X PUT -d aaf http://localhost:8080/api/admin/settings/:DefaultAuthProvider
+
+# Set AuScope logo
+cp /custom-ui/AuScopeLogoIcon.png /opt/payara/appserver/glassfish/domains/domain1/applications/dataverse/resources/images
+curl -X PUT -d '/resources/images/AuScopeLogoIcon.png' http://localhost:8080/api/admin/settings/:LogoCustomizationFile
